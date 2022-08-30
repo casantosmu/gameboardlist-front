3d638fde4d2eac0e28cdc63307a20f9be3f2944f
Carlos <casantosmu@gmail.com>
2022-08-30 11:46:15 +0200

    Added styles object && Aded Theme component

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import App from "./App";
+import Theme from "./styles/Theme/Theme";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -10,7 +11,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Theme>
+        <App />
+      </Theme>
     </Provider>
   </React.StrictMode>
 );
---
+++ src/styles/styles.ts
@@ -0,0 +1,25 @@
+const styles = {
+  colors: {
+    brand: {
+      normal: "#1890ff",
+      hover: "#40a9ff",
+      click: "#096dd9",
+    },
+    functional: {
+      link: "#1890ff",
+      success: "#52c41a",
+      warning: "#faad14",
+      error: "#ff4d4f",
+    },
+    neutral: {
+      title: "#262626",
+      primaryText: "#262626",
+      secondaryText: "#8c8c8c",
+      disable: "#bfbfbf",
+      border: "#d9d9d9",
+      background: "#f5f5f5",
+    },
+  },
+};
+
+export default styles;
