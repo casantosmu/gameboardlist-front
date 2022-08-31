7e089e9da56c78b38d8ff4e834cfabe9b7766e8b
Carlos <casantosmu@gmail.com>
2022-08-31 20:38:05 +0200

    Added test for Register Page

---
+++ src/pages/RegisterPage/RegisterPage.test.tsx
@@ -0,0 +1,29 @@
+import { screen } from "@testing-library/react";
+import renderWithProviders from "../../utils/test-utils";
+import RegisterPage from "./RegisterPage";
+
+describe("Given a registerForm", () => {
+  describe("When its rendered", () => {
+    test("Then it should render a heading with 'Register'", () => {
+      const expectedText = "Register";
+
+      renderWithProviders(<RegisterPage />);
+
+      const heading = screen.getByRole("heading", {
+        name: expectedText,
+      });
+
+      expect(heading).toBeInTheDocument();
+    });
+
+    test("Then it should render a register form", () => {
+      const expectedFormName = "Register form";
+
+      renderWithProviders(<RegisterPage />);
+
+      const heading = screen.getByRole("form", { name: expectedFormName });
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
+});
