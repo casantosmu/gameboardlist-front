aa560db0697647b0d27bec63064a6adb649e4aa1
Carlos <casantosmu@gmail.com>
2022-09-04 00:47:57 +0200

    Added post login msw handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,37 +1,24 @@
-// src/mocks/handlers.js
 import { rest } from "msw";
+import config from "../config";
+
+const baseUrl = config.endpoints.base;
 
 export const handlers = [
-  rest.post("/login", (req, res, ctx) => {
-    // Persist user's authentication in the session
-    sessionStorage.setItem("is-authenticated", "true");
+  rest.post(
+    `${baseUrl}${config.endpoints.loginPath}`,
+    async (req, res, ctx) => {
+      const request = await req.json();
 
-    return res(
-      // Respond with a 200 status code
-      ctx.status(200)
-    );
-  }),
-
-  rest.get("/user", (req, res, ctx) => {
-    // Check if the user is authenticated in this session
-    const isAuthenticated = sessionStorage.getItem("is-authenticated");
-
-    if (!isAuthenticated) {
-      // If not authenticated, respond with a 403 error
-      return res(
-        ctx.status(403),
-        ctx.json({
-          errorMessage: "Not authorized",
-        })
-      );
+      return request.user.email === "exists"
+        ? res(ctx.status(400), ctx.json(null))
+        : res(
+            ctx.status(201),
+            ctx.json({
+              user: {
+                token: "token",
+              },
+            })
+          );
     }
-
-    // If authenticated, return a mocked user details
-    return res(
-      ctx.status(200),
-      ctx.json({
-        username: "admin",
-      })
-    );
-  }),
+  ),
 ];
