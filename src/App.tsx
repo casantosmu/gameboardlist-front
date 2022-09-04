5568294d179cd9afa95e811308a2ee81018cd744
Carlos <casantosmu@gmail.com>
2022-09-04 14:45:37 +0200

    Added LoginPage route to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<h1>Hola mundo</h1>} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
