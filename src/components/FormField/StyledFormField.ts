9bdcabd1df06fb686cbaa35a1721c2b036184d71
Carlos <casantosmu@gmail.com>
2022-09-04 15:02:51 +0200

    Added icons on FormFields

--- src/components/FormField/StyledFormField.ts
+++ src/components/FormField/StyledFormField.ts
@@ -1,17 +1,41 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const StyledFormField = styled.div`
+const iconStyles = css`
+  .form-field__input-wrapper {
+    position: relative;
+  }
+
+  .form-field__icon {
+    position: absolute;
+    left: 1em;
+    top: 50%;
+    transform: translateY(-50%);
+    color: ${({ theme }) => theme.colors.brand.normal};
+  }
+
+  input {
+    padding-left: 2.5em;
+  }
+`;
+
+interface StyledButtonProps {
+  icon: boolean;
+}
+
+const StyledFormField = styled.div<StyledButtonProps>`
   margin-bottom: 1.5rem;
 
   .form-field__label {
     display: block;
-    margin-bottom: 0.3em;
+    padding-bottom: 0.5em;
   }
 
   .form-field__required {
     color: ${({ theme }) => theme.colors.functional.error};
     margin-right: 5px;
   }
+
+  ${(props) => props.icon && iconStyles};
 `;
 
 export default StyledFormField;
