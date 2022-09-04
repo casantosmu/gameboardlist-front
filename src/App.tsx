a65e08482f2ddf63096692174936ba1055c538a8
Carlos <casantosmu@gmail.com>
2022-09-04 18:10:21 +0200

    Added NotFoundPage component and styles && Added NotFound route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 library.add(fas);
 
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<h1>Hola mundo</h1>} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
