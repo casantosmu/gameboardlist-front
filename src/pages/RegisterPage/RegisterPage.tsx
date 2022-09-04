1056aef504be9de9fd0c555c2d640ed0dde1bc90
Carlos <casantosmu@gmail.com>
2022-09-04 15:27:22 +0200

    Added icon to go back to login on RegisterPage

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,12 +1,28 @@
 import Container from "../../components/Container/Container";
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
 import StyledRegisterPage from "./StyledRegisterPage";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import VisuallyHidden from "../../components/VisuallyHidden/VisuallyHidden";
 
 const RegisterPage = (): JSX.Element => {
   return (
     <StyledRegisterPage>
       <Container breakpoint="small">
-        <h1 className="register-page__heading">Register</h1>
+        <div className="register-page__top">
+          <h1 className="register-page__heading">Register</h1>
+          <Link to={"/login"}>
+            <FontAwesomeIcon
+              icon={faXmark}
+              size="xl"
+              className="register-page__close"
+              title="Back to login page"
+              aria-hidden="true"
+            />
+            <VisuallyHidden>Back to login</VisuallyHidden>
+          </Link>
+        </div>
         <RegisterForm />
       </Container>
     </StyledRegisterPage>
--- src/pages/RegisterPage/StyledRegisterPage.ts
+++ src/pages/RegisterPage/StyledRegisterPage.ts
@@ -5,14 +5,24 @@ const StyledRegisterPage = styled.div`
   display: flex;
   align-items: center;
 
-  .register-page__heading {
-    padding-bottom: 1rem;
+  .register-page__top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-bottom: 0.75rem;
     margin-bottom: 1rem;
-    margin-top: 0;
+    border-bottom: 2px solid ${({ theme }) => theme.colors.brand.normal};
+  }
+
+  .register-page__close {
+    color: ${({ theme }) => theme.colors.neutral.secondaryText};
+  }
+
+  .register-page__heading {
+    margin: 0;
     font-weight: 400;
     font-size: inherit;
     color: ${({ theme }) => theme.colors.brand.normal};
-    border-bottom: 2px solid ${({ theme }) => theme.colors.brand.normal};
   }
 `;
 
