dbddae19291ad463a5abf34b20b685730556a0b4
Carlos <casantosmu@gmail.com>
2022-09-05 12:14:23 +0200

    Modified FetchApi test to be more clear

--- src/services/FetchApi.test.ts
+++ src/services/FetchApi.test.ts
@@ -4,11 +4,16 @@ describe("Given a FetchApi class", () => {
   const fetchApi = new FetchApi();
 
   describe("When instantated and invoked its method loginUser with a user", () => {
-    describe("And the fetch to login endpoint with a POST method and a user respone with ok status", () => {
+    describe("And the fetch to login endpoint with a POST method and a user respone with a ok status", () => {
       test("Then it should resolve with the user token", async () => {
         const user = {
           email: "email",
           password: "password",
+        };
+        const expectedResponse = {
+          user: {
+            token: "token",
+          },
         };
 
         let result: any;
@@ -18,18 +23,12 @@ describe("Given a FetchApi class", () => {
           result = error;
         }
 
-        const expectedResponse = {
-          user: {
-            token: "token",
-          },
-        };
-
         expect(result).toStrictEqual(expectedResponse);
       });
     });
 
     describe("And the fetch to login endpoint with a POST method and a user respone with 'Bad Request'", () => {
-      test("Then it should reject with an error with 'Bad request'", async () => {
+      test("Then it should reject with an error with 'Bad Request'", async () => {
         const user = {
           email: "exists",
           password: "password",
