51e2da44398aea856f97a12e40601bb87b4e9e3b
Carlos <casantosmu@gmail.com>
2022-09-08 23:36:20 +0200

    Added gameboardsSlice to the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,10 +2,12 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import type { PreloadedState } from "@reduxjs/toolkit";
 import userSlice from "./slices/userSlice/userSlice";
 import uiSlice from "./slices/uiSlice/uiSlice";
+import gameboardsSlice from "./slices/gameboardsSlice/gameboardsSlice";
 
 const rootReducer = combineReducers({
   user: userSlice,
   ui: uiSlice,
+  gameboards: gameboardsSlice,
 });
 
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
