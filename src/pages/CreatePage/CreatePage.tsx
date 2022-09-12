0058bb57174fdf2c720f44a254bba1ed0cf9d3ec
Carlos <casantosmu@gmail.com>
2022-09-12 17:44:24 +0200

    Added CreatePage component, styles and route

---
+++ src/pages/CreatePage/CreatePage.tsx
@@ -0,0 +1,16 @@
+import Container from "../../components/Container/Container";
+import CreateForm from "../../components/CreateForm/CreateForm";
+import StyledCreatePage from "./StyledCreatePage";
+
+const CreatePage = () => {
+  return (
+    <Container breakpoint="large" style={{ padding: "1rem 0 2rem" }}>
+      <StyledCreatePage>
+        <h1 className="create-page__heading">Add new</h1>
+        <CreateForm />
+      </StyledCreatePage>
+    </Container>
+  );
+};
+
+export default CreatePage;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import Container from "../../components/Container/Container";
 import GameboardsGrid from "../../components/GameboardsGrid/GameboardsGrid";
@@ -10,17 +11,18 @@ import StyledHomePage from "./StyledHomePage";
 const HomePage = (): JSX.Element => {
   const { getGameboards } = useGameboards();
   const { gameboards } = useAppSelector((state) => state.gameboards);
+  const navigate = useNavigate();
 
   useEffect(() => {
     getGameboards();
   }, [getGameboards]);
 
   return (
-    <StyledHomePage>
-      <Container breakpoint="large">
+    <Container breakpoint="large" style={{ paddingBottom: "2rem" }}>
+      <StyledHomePage>
         <header className="home-page__header">
           <h1 className="home-page__heading">Your collection</h1>
-          <Button onClick={() => {}}>
+          <Button onClick={() => navigate("/create")}>
             <FontAwesomeIcon
               icon="plus"
               aria-hidden="true"
@@ -30,8 +32,8 @@ const HomePage = (): JSX.Element => {
           </Button>
         </header>
         <GameboardsGrid gameboards={gameboards} />
-      </Container>
-    </StyledHomePage>
+      </StyledHomePage>
+    </Container>
   );
 };
 
