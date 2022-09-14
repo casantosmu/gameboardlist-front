dcf2450dad987262059af874ced8d936d630d76c
Carlos <casantosmu@gmail.com>
2022-09-14 10:57:45 +0200

    Added seconday color on Button

--- src/components/Button/StyledButton.ts
+++ src/components/Button/StyledButton.ts
@@ -1,14 +1,13 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const StyledButton = styled.button`
-  display: inline-block;
-  padding: 0.75em 1em;
+interface ButtonProps {
+  semantic: "primary" | "secondary";
+}
+
+const PrimaryButton = css`
   background-color: ${({ theme }) => theme.colors.brand.normal};
   color: ${({ theme }) => theme.colors.neutral.light};
-  text-align: center;
-  border-radius: 2px;
   border: 1px solid ${({ theme }) => theme.colors.brand.normal};
-  cursor: pointer;
 
   &:hover {
     background-color: ${({ theme }) => theme.colors.brand.hover};
@@ -16,13 +15,47 @@ const StyledButton = styled.button`
 
   &:active {
     background-color: ${({ theme }) => theme.colors.brand.click};
-    outline: none;
   }
 
   &:focus {
     outline: 1px solid ${({ theme }) => theme.colors.brand.hover};
     box-shadow: inset 0px 0px 0px 1px ${({ theme }) => theme.colors.brand.click};
   }
+`;
+
+const SecondaryButton = css`
+  background-color: ${({ theme }) => theme.colors.neutral.light};
+  color: ${({ theme }) => theme.colors.neutral.primaryText};
+  border: 1px solid ${({ theme }) => theme.colors.neutral.border};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.neutral.lightHover};
+  }
+
+  &:active {
+    background-color: ${({ theme }) => theme.colors.neutral.lightClick};
+  }
+
+  &:focus {
+    outline: 1px solid ${({ theme }) => theme.colors.neutral.lightHover};
+    box-shadow: inset 0px 0px 0px 1px
+      ${({ theme }) => theme.colors.neutral.lightClick};
+  }
+`;
+
+const StyledButton = styled.button<ButtonProps>`
+  display: inline-block;
+  padding: 0.75em 1em;
+  text-align: center;
+  border-radius: 2px;
+  cursor: pointer;
+
+  &:active {
+    outline: none;
+  }
+
+  ${({ semantic }) => semantic === "primary" && PrimaryButton}
+  ${({ semantic }) => semantic === "secondary" && SecondaryButton}
 `;
 
 export default StyledButton;
