fa7002b070ebc8a36dfacb0d032a12282ee91b42
Carlos <casantosmu@gmail.com>
2022-09-25 00:26:59 +0200

    Added getGameboard method on FetchApi and tested

--- src/services/FetchApi.test.ts
+++ src/services/FetchApi.test.ts
@@ -1,5 +1,5 @@
 import { UserLogin, UserRegister } from "../types/interfaces";
-import { fakeGameboards } from "../utils/mocks/fakeData";
+import { fakeGameboard1, fakeGameboards } from "../utils/mocks/fakeData";
 import FetchApi from "./FetchApi";
 
 describe("Given a FetchApi class", () => {
@@ -154,6 +154,24 @@ describe("Given a FetchApi class", () => {
         }
 
         expect(result).toBe(expectedResult);
+      });
+    });
+  });
+
+  describe("When instantated and invoked its method getGameboard with a token and an id", () => {
+    describe("And the GET fetch to gameboards endpoint with an id, with an auth token, respone with a ok status", () => {
+      test("Then it should resolve with gameboard data", async () => {
+        const id = "id";
+        const token = "token";
+
+        let result: unknown;
+        try {
+          result = await fetchApi.getGameboard(token, id);
+        } catch (error) {
+          result = error;
+        }
+
+        expect(result).toStrictEqual(fakeGameboard1);
       });
     });
   });
